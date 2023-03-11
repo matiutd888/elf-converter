048e35155043393151835f09e18c784c1629dfdf
Mateusz <matiutd888@gmail.com>
2023-03-11 17:52:10 +0100

    Add elfio library

---
+++ src/converter.h
@@ -0,0 +1,8 @@
+//
+// Created by mateusz on 11.03.23.
+//
+
+#ifndef LA1_CONVERTER_H
+#define LA1_CONVERTER_H
+
+#endif //LA1_CONVERTER_H
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by mateusz on 11.03.23.
+//
+
+#include <iostream>
+#include <elfio/elfio.hpp>
+
+int main() {
+    std::cout << "hello world!";
+}
+
