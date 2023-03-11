6222cb5e7cab098274836d0e1d22abad77d71d0e
Mateusz <matiutd888@gmail.com>
2023-03-11 21:57:32 +0100

    Update keystone

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
-//
-// Created by mateusz on 11.03.23.
-//
-
 #include <iostream>
 #include <elfio/elfio.hpp>
+#include <capstone/capstone.h>
+#include <keystone/keystone.h>
 
-int main() {
-    std::cout << "hello world!";
+using namespace ELFIO;
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cout << "Usage: tutorial <elf_file>" << std::endl;
+        return 1;
+    }
+
+    elfio reader;
+
+    if (!reader.load(argv[1])) {
+        std::cout << "Can't find or process ELF file " << argv[1] << std::endl;
+    }
+
+
+    std::cout << reader.get_elf_version() << std::endl;
+
+        // 1. przeiteruj po sekcjach i usuń te o nazwie .note.gnu property *.eh_frame
+        //
 }
-
