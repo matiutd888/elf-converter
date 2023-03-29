3acab99b3b43c0d6020174d2606cdef9837ca5da
Mateusz <matiutd888@gmail.com>
2023-03-29 05:25:07 +0200

    fix prepare solution.sh

--- prepare-solution.sh
+++ prepare-solution.sh
@@ -1,2 +1,6 @@
 rm -rf mn418323.zip
+mv CMakeLists.txt real-CMakeLists.txt
+mv CMakeLists-for-solution.txt CMakeLists.txt
 zip -r mn418323.zip  README.md CMakeLists.txt src/*.cpp src/*.h --exclude src/build
+mv CMakeLists.txt CMakeLists-for-solution.txt
+mv real-CMakeLists.txt CMakeLists.txt
